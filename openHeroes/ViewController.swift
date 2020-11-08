aaba70202b5cfd9bbe1dbe58ce69bebb67c4af8b
Simon <simon@aparicio.org>
2020-11-08 23:21:19 +0100

    Xcode project created

---
+++ openHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//  ViewController.swift
+//  openHeroes
+//
+//  Created by Simón Aparicio on 08/11/2020.
+//  Copyright © 2020 iPon.es All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
