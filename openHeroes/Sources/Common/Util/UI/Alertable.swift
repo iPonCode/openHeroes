024226f6570bfeb4b286dc2fae0fa5eae1b6162d
Simon <simon@aparicio.org>
2020-11-16 05:22:26 +0100

    * Improve Error control for loadError 	* Additional check on Alertable protocol to prevent dismiss accidentally non UIAlertController

--- openHeroes/Sources/Common/Util/UI/Alertable.swift
+++ openHeroes/Sources/Common/Util/UI/Alertable.swift
@@ -16,15 +16,13 @@ extension Alertable {
     
     func showAlert(msg: String, title: String) {
         
-        // If there was already a viewController presented, dismiss it and present the new one:
+        // If there was already an AlertController presented, dismiss it and present the new one:
         //      by example: Two Errors showns consecutively
-        //      TODO: Add condition checking if also isKindOfClass UIAlertViewController
-        if view?.presentedViewController != nil {
-            view?.presentedViewController?.dismiss(animated: true) { presentAlert(msg: msg, title: title) }
-        } else {
+        guard let isAlert = view?.presentedViewController?.isKind(of: UIAlertController.self), isAlert else {
             presentAlert(msg: msg, title: title)
+            return
         }
-        
+        view?.presentedViewController?.dismiss(animated: true) { presentAlert(msg: msg, title: title) }
     }
     
     private func presentAlert(msg: String, title: String) {
